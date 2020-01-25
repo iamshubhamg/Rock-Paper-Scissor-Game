28543d9131017744361f8f8051119feeaa4df04b
Shubham Giri <53148932+iamshubhamg@users.noreply.github.com>
2020-01-25 11:11:08 +0530

    Update game.py

--- game.py
+++ game.py
@@ -16,6 +16,19 @@ if choice == 1:
         choice_name = 'paper'
     else: 
         choice_name = 'scissor'
-        
         print("user choice is: " + choice_name) 
     print("\nNow its computer turn.......") 
+    comp_choice = random.randint(1, 3) 
+     while comp_choice == choice: 
+        comp_choice = random.randint(1, 3) 
+         if comp_choice == 1: 
+        comp_choice_name = 'Rock'
+    elif comp_choice == 2: 
+        comp_choice_name = 'paper'
+    else: 
+        comp_choice_name = 'scissor'
+          
+    print("Computer choice is: " + comp_choice_name) 
+  
+    print(choice_name + " V/s " + comp_choice_name) 
+  #condition for winning
